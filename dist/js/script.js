7e5200a7fd97fa48507c9505c2d5330ddfbf0ae1
payal <payal.priyadarshini@gmail.com>
2019-07-11 12:25:16 +0100

    Implemented the Hamburger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,5 @@
 
+/*
 
 function showPic1()
 {
@@ -9,31 +10,29 @@ function showPic1()
     document.getElementById("place").innerHTML = "US";
 }
 
-
-function showPic2()
-{
-    document.getElementById("bigPic").style.backgroundImage='url("./img/newLayout/news2.jpg")';
-    document.getElementById("p1").innerHTML = "Chancellor-Commandant nomination";
-    document.getElementById("ht").innerHTML = "Polish President grants generals nominations - wsosp.pl";
-    document.getElementById("time").innerHTML = "4 hrs";
-    document.getElementById("place").innerHTML = "Poland";
-}
-
-
-function showPic3()
-{
-    document.getElementById("bigPic").style.backgroundImage='url("./img/newLayout/news3.jpg")';
-    document.getElementById("p1").innerHTML = "Deepika turns down film with Shahrukh";
-    document.getElementById("ht").innerHTML = "The pair will not be starring in next film - BritAsia TV";
-    document.getElementById("time").innerHTML = "3 hrs";
-    document.getElementById("place").innerHTML = "Mumbai";
-}
-
+ /**
+     *  To goto Home Page when the logo is clicked
+     */
 function showHome()
 {
     window.open("index.html", "_self");
-
 }
+/**
+ * Open the Navigation sidebar when Hamburger icon is clicked.
+ */
+function openNav() {
+    document.getElementById("nav1").style.width = "360px";
+    document.getElementById("top").style.marginLeft = "360px";
+}
+
+/**
+ * Close the Navigation sidebar when cross symbol is clicked.
+ */
+function closeNav() {
+    document.getElementById("nav1").style.width = "0";
+    document.getElementById("top").style.marginLeft= "0";
+}
+
 
 
 /*
@@ -48,12 +47,35 @@ function showHome()
 */
 
 (function(){
-    const bigPic = document.getElementById("bigPic");
-    const p1 = document.getElementById("p1");
-    const ht = document.getElementById("ht");
 
-    //onload initialiser
-    window.onload = init;
+    /* const ht = document.getElementById("ht");*/
+
+
+
+    /**
+     *  Bind fields to event handler
+     */
+    function bindBtns() {
+        /* //Checking if button exists on page and then add eventhandler
+         /!*Open Trump page when pic is clicked*!/
+         if(bigPic != null) bigPic.addEventListener("click", openTrumpPage );*/
+    }// bindBtns
+
+    //Functions
+
+    /**
+     *  To Display the chosen destination on
+     *  displaydestination.html page
+     */
+    /* function openTrumpPage()
+     {
+        /!* window.open("trump.html", "_self");*!/
+     }*/
+
+
+
+
+
 
     /**
      * initialising function
@@ -63,31 +85,7 @@ function showHome()
 
     }//end init
 
-    /**
-     *  Bind fields to event handler
-     */
-    function bindBtns() {
-        //Checking if button exists on page and then add eventhandler
-        /*Open Trump page when pic is clicked*/
-        if(bigPic != null) bigPic.addEventListener("click", openTrumpPage );
-
-        /*Open Trump page when headline is clicked*/
-        if(p1 != null) p1.addEventListener("click", openTrumpPage );
-        /*Open Trump page when headlinetext is clicked*/
-        if(ht != null) ht.addEventListener("click",openTrumpPage );
-    }// bindBtns
-
-    //Functions
-
-    /**
-     *  To Display the chosen destination on
-     *  displaydestination.html page
-     */
-    function openTrumpPage()
-    {
-        window.open("trump.html", "_self");
-
-    }
-
+    //onload initialiser
+    window.onload = init;
 })();
 
